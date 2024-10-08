696bd9f831f21d32f1d18b23400d30259fab1b3a
Cliff Chen <cfchen@futuredial.com>
2024-10-08 23:03:33 +0800

    Add utils

--- utils.lua
+++ utils.lua
@@ -21,3 +21,40 @@ function isInParty(pid)
     end
     return false
 end
+
+function array_contains(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+function string_split(s, p)
+    local temp = {}
+    local index = 0
+    local last_index = string.len(s)
+
+    while true do
+        local i, e = string.find(s, p, index)
+
+        if i and e then
+            local next_index = e + 1
+            local word_bound = i - 1
+            table.insert(temp, string.sub(s, index, word_bound))
+            index = next_index
+            log(index)
+        else            
+            if index > 0 and index <= last_index then
+                table.insert(temp, string.sub(s, index, last_index))
+            elseif index == 0 then
+                temp = nil
+            end
+            break
+        end
+    end
+
+    return temp
+end
