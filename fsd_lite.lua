04e99fe0946d31676cd8d4c4e476910a20c330f6
Cliff Chen <cfchen@futuredial.com>
2024-12-10 17:40:20 +0800

    Add fsd gr

--- fsd_lite.lua
+++ fsd_lite.lua
@@ -7,6 +7,12 @@ function fsd_go(app, path, callback)
     windower.send_command("input //fsd g "..path.." "..key.." "..app)
 end
 
+function fsd_go_reverse(app, path, callback)
+    key = tostring(os.clock()*1000)
+    callback_table[key] = callback
+    windower.send_command("input //fsd gr "..path.." "..key.." "..app)
+end
+
 windower.register_event('addon command', function(command, ...)
     if string.find(command, 'FSD_callback:') == 1 then
         key = string.sub(command, 14, -1)
